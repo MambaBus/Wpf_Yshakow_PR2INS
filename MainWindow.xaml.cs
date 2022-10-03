ee78c4cfff32a4f54d6ba1294d3c97e8589d5683
Алексей Писарев <afonasiu2983@gmail.com>
2022-10-03 11:51:59 +1000

    Пример

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,6 +27,15 @@ namespace Wpf_Yshakow_PR3
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Window1 window1 = new Window1();
+            window1.Show();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
 
         }
     }
--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -22,6 +22,39 @@ namespace Wpf_Yshakow_PR3
         public Window1()
         {
             InitializeComponent();
+            X.Text = "5,5";
+            Y.Text = "4,3";
+            Z.Text = "2,8";
+            Itog.Text = "Ст. гр. ИСП Ушаков С.А";
+        }
+
+        private void Schet_Click(object sender, RoutedEventArgs e)
+        {
+            Itog.Text += Environment.NewLine + "Практическая работа 2";
+            // Считывание значения X
+            double x = double.Parse(X.Text);
+            // Вывод значения X в окно
+            Itog.Text += Environment.NewLine + "X = " + x.ToString();
+
+            // Считывание значения X
+            double y = double.Parse(Y.Text);
+            // Вывод значения Y в окно
+            Itog.Text += Environment.NewLine + "Y = " + y.ToString();
+
+            // Считывание значения X
+            double z = double.Parse(Z.Text);
+            // Вывод значения Z в окно
+            Itog.Text += Environment.NewLine + "Z = " + z.ToString();
+
+            // Вычисляем арифметическое выражение
+            double a = Math.Tan(x + y) * Math.Tan(x + y);
+            double b = Math.Exp(y - z);
+            double c = Math.Sqrt(Math.Cos(x * x) + Math.Sin(z * z));
+            double u = a - b * c;
+
+            //Выводим результат в окно
+            Itog.Text += Environment.NewLine + "Результат U = " + u.ToString();
+          
         }
     }
 }
